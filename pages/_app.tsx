1163dbdf75b46dcdbfd9017a26e28b70b19fd457
sandeepsharma09 <sandeepsharma999000@gmail.com>
2023-07-20 15:31:50 +0530

    first commit1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import AnalyticsProvider, {
   initializeAnalytics,
 } from 'components/AnalyticsProvider'
 initializeAnalytics()
-
+import "./../styles/global.css";
 import { Inter } from '@next/font/google'
 import type { AppContext, AppProps } from 'next/app'
 import { default as NextApp } from 'next/app'
@@ -11,14 +11,15 @@ import { darkTheme, globalReset } from 'stitches.config'
 import '@rainbow-me/rainbowkit/styles.css';
 
 import {
-  RainbowKitProvider,
+
   getDefaultWallets,
+
   darkTheme as rainbowDarkTheme,
   lightTheme as rainbowLightTheme,
 } from '@rainbow-me/rainbowkit'
-import { WagmiConfig, createClient, configureChains } from 'wagmi'
+
 import * as Tooltip from '@radix-ui/react-tooltip'
-import { publicProvider } from 'wagmi/providers/public'
+
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 
 import {
@@ -34,8 +35,30 @@ import ToastContextProvider from 'context/ToastContextProvider'
 import supportedChains from 'utils/chains'
 import { useMarketplaceChain } from 'hooks'
 import ChainContextProvider from 'context/ChainContextProvider'
-import { ThirdwebProvider } from '@thirdweb-dev/react'
+
 import { Mumbai, Goerli,Ethereum, Arbitrum, ArbitrumGoerli } from "@thirdweb-dev/chains";
+////////////start/////////////
+
+import {
+  configureChains,
+  createClient,
+  WagmiConfig,
+  useSigner,
+  mainnet,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import {
+  connectorsForWallets,
+  wallet,
+  RainbowKitProvider,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ThirdwebSDKProvider, ChainId } from "@thirdweb-dev/react";
+import { optimism, polygon } from 'wagmi/dist/chains';
+
+////////////end/////////////
 //CONFIGURABLE: Use nextjs to load your own custom font: https://nextjs.org/docs/basic-features/font-optimization
 const inter = Inter({
   subsets: ['latin'],
@@ -45,21 +68,21 @@ export const NORMALIZE_ROYALTIES = process.env.NEXT_PUBLIC_NORMALIZE_ROYALTIES
   ? process.env.NEXT_PUBLIC_NORMALIZE_ROYALTIES === 'true'
   : false
 
-const { chains, provider } = configureChains(supportedChains, [
-  alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID || '' }),
-  publicProvider(),
-])
+// const { chains, provider } = configureChains(supportedChains, [
+//   alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID || '' }),
+//   publicProvider(),
+// ])
 
-const { connectors } = getDefaultWallets({
-  appName: 'Reservoir Marketplace',
-  chains,
-})
+// const { connectors } = getDefaultWallets({
+//   appName: 'Reservoir Marketplace',
+//   chains,
+// })
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-})
+// const wagmiClient = createClient({
+//   autoConnect: true,
+//   connectors,
+//   provider,
+// })
 
 //CONFIGURABLE: Here you can override any of the theme tokens provided by RK: https://docs.reservoir.tools/docs/reservoir-kit-theming-and-customization
 const reservoirKitThemeOverrides = {
@@ -69,6 +92,91 @@ const reservoirKitThemeOverrides = {
   primaryHoverColor: '#644fc1',
 }
 
+
+////////////////////////start/////////////////
+
+
+
+// const { chains, provider } = configureChains(
+//   [chain.goerli],
+//   [
+//     jsonRpcProvider({
+//       rpc: () => {
+//         return {
+//           http: "https://rpc.ankr.com/eth_goerli",
+//         };
+//       },
+//     }),
+//     publicProvider(),
+//   ]
+// );
+
+const { chains, provider } = configureChains(
+  [mainnet, optimism, polygon],
+  [
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/eth_goerli",
+        };
+      },
+    }),
+    publicProvider(),
+  ]
+);
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      wallet.metaMask({ chains, shimDisconnect: true }),
+      wallet.walletConnect({ chains }),
+      wallet.coinbase({ appName: "probably nothing", chains }),
+      wallet.rainbow({ chains }),
+    ],
+  },
+  {
+    groupName: "Others",
+    wallets: [
+      wallet.argent({ chains }),
+      wallet.brave({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.imToken({ chains }),
+      wallet.injected({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.ledger({
+        chains,
+      }),
+      wallet.steak({ chains }),
+      wallet.trust({ chains, shimDisconnect: true }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
+
+const activeChainId = ChainId.Goerli;
+function ThirdwebProvider({ wagmiClient, children }: any) {
+  const { data: signer } = useSigner();
+  return (
+    <ThirdwebSDKProvider
+      desiredChainId={activeChainId}
+      signer={signer as any}
+      provider={wagmiClient.provider}
+      queryClient={wagmiClient.queryClient as any}
+    >
+      {children}
+    </ThirdwebSDKProvider>
+  );
+}
+////////////////////////end/////////////////
 function AppWrapper(props: AppProps & { baseUrl: string }) {
 
   const activeChain = "mumbai"; 
@@ -84,17 +192,17 @@ function AppWrapper(props: AppProps & { baseUrl: string }) {
     >
       {/* // activeChain={activeChain} */}
       <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={midnightTheme()} coolMode>
+          <ThirdwebProvider wagmiClient={wagmiClient}>
         <ChainContextProvider>
-          <ThirdwebProvider 
           
-          supportedChains={[Mumbai,Goerli,Ethereum,Arbitrum,ArbitrumGoerli]
-        }
-          >
           <AnalyticsProvider>
             <MyApp {...props} />
           </AnalyticsProvider>
-          </ThirdwebProvider>
+          
         </ChainContextProvider>
+        </ThirdwebProvider>
+      </RainbowKitProvider>
       </WagmiConfig>
     </ThemeProvider>
   )
